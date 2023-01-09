2fde5fea0bae0c846205758ddd08264b26a7144f
Addis Belete <addis.bel@gmail.com>
2023-01-09 20:51:28 +0300

    Add a UserInfo struct and userInfo mapping

--- src/Core/LP.sol
+++ src/Core/LP.sol
@@ -3,13 +3,24 @@ pragma solidity ^0.8.13;
 
 /**
  * @notice LP get 7% Interest in 100days
+ * user provide liquidity to Better Barter by using this contract.
  */
 contract LP {
-    //user provide liquidity to Better Barter by using this contract.
+    /**
+     * @notice Struct that holds liquidity provider info
+     */
+    struct UserInfo {
+        uint256 initialTime;
+        uint256 stakingPeriod;
+        uint256 amount;
+        uint256 interestAccumulated;
+    }
 
+    mapping(address => mapping(uint256 => UserInfo)) public userInfo; // address => stakeId -> UserInfo
     /**
      * @notice User will provide liquidity by using this deposit function
      */
+
     function deposit() external {}
 
     /**
