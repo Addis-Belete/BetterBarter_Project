a29cc39ac0bcf63c6cfad9a5e37e4563a2e779e9
Addis Belete <addis.bel@gmail.com>
2023-01-09 16:22:47 +0300

    Add a receiptToken.sol

---
+++ src/Core/LP.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract LP {
+    //user provide liquidity to Better Barter by using this contract.
+
+    /**
+     * @notice User will provide liquidity by using this deposit function
+     */
+    function deposit() external {}
+
+    /**
+     * @notice User will withdraw funds by using this function once the staking period ends.
+     */
+    function withdraw() external {}
+
+    function claimInterest() external {}
+}
