3210e977820250230963a3641657d098762557d3
Addis Belete <addis.bel@gmail.com>
2023-01-09 20:57:13 +0300

    Add a constractor

--- src/Core/LP.sol
+++ src/Core/LP.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "../../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "../Interfaces/IReceiptToken.sol";
 /**
  * @notice LP get 7% Interest in 100days
  * user provide liquidity to Better Barter by using this contract.
  */
+
 contract LP {
     /**
      * @notice Struct that holds liquidity provider info
@@ -17,10 +20,18 @@ contract LP {
     }
 
     mapping(address => mapping(uint256 => UserInfo)) public userInfo; // address => stakeId -> UserInfo
+    mapping(address => uint256) public lastStakingId; //address => lastStaking Id
+    IReceiptToken internal receiptToken;
+    IERC20 internal underlying;
+
+    constructor(address _receiptTokenAddress, address _underlyingAddress) {
+        receiptToken = IReceiptToken(_receiptTokenAddress);
+        underlying = IERC20(_underlyingAddress);
+    }
+
     /**
      * @notice User will provide liquidity by using this deposit function
      */
-
     function deposit() external {}
 
     /**
